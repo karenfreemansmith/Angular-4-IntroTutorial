ebdb795997dec5785a6dcf31d4e74d226237ad99
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-03-14 22:49:42 -0700

    add routing and services, characters not showing-but no error

---
+++ src/app/classes/data-characters.ts
@@ -0,0 +1,104 @@
+import { Character } from './character';
+
+export const CHARACTERS: Character[] = [
+    {
+        "name": "R5-D4",
+        "height": "97",
+        "mass": "32",
+        "hair_color": "n/a",
+        "skin_color": "white, red",
+        "eye_color": "red",
+        "birth_year": "unknown",
+        "gender": "n/a"
+    }, {
+        "name": "Biggs Darklighter",
+        "height": "183",
+        "mass": "84",
+        "hair_color": "black",
+        "skin_color": "light",
+        "eye_color": "brown",
+        "birth_year": "24BBY",
+        "gender": "male"
+    }, {
+        "name": "Beru Whitesun lars",
+        "height": "165",
+        "mass": "75",
+        "hair_color": "brown, grey",
+        "skin_color": "light",
+        "eye_color": "blue",
+        "birth_year": "52BBY",
+        "gender": "female"
+    }, {
+        "name": "Owen Lars4",
+        "height": "178",
+        "mass": "120",
+        "hair_color": "brown",
+        "skin_color": "light",
+        "eye_color": "blue",
+        "birth_year": "47BBY",
+        "gender": "male"
+    }, {
+        "name": "Jek Tono Porkins",
+        "height": "180",
+        "mass": "110",
+        "hair_color": "brown",
+        "skin_color": "fair",
+        "eye_color": "blue",
+        "birth_year": "unknown",
+        "gender": "male"
+    }, {
+        "name": "Obi-Wan Kenobi",
+        "height": "182",
+        "mass": "77",
+        "hair_color": "auburn, white",
+        "skin_color": "fair",
+        "eye_color": "blue-gray",
+        "birth_year": "57BBY",
+        "gender": "male"
+    }, {
+        "name": "Shmi Skywalker",
+        "height": "163",
+        "mass": "unknown",
+        "hair_color": "black",
+        "skin_color": "fair",
+        "eye_color": "brown",
+        "birth_year": "72BBY",
+        "gender": "female"
+    }, {
+        "name": "Darth Maul",
+        "height": "175",
+        "mass": "80",
+        "hair_color": "none",
+        "skin_color": "red",
+        "eye_color": "yellow",
+        "birth_year": "54BBY",
+        "gender": "male"
+    }, {
+        "name": "Luke Skywalker",
+        "height": "172",
+        "mass": "77",
+        "hair_color": "blond",
+        "skin_color": "fair",
+        "eye_color": "blue",
+        "birth_year": "19BBY",
+        "gender": "male"
+    }, {
+        "name": "Yoda",
+        "height": "66",
+        "mass": "17",
+        "hair_color": "white",
+        "skin_color": "green",
+        "eye_color": "brown",
+        "birth_year": "896BBY",
+        "gender": "male"
+    }, {
+        "name": "Dooku",
+        "height": "193",
+        "mass": "80",
+        "hair_color": "white",
+        "skin_color": "fair",
+        "eye_color": "brown",
+        "birth_year": "102BBY",
+        "gender": "male"
+    },
+];
---
+++ src/app/classes/data-movies.ts
@@ -0,0 +1,46 @@
+import { Movie } from './movie';
+
+export const MOVIES: Movie[] = [
+    {"title":"A New Hope", "episode_id":4,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    },
+    {"title":"The Empire Strikes Back", "episode_id":5,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    },
+    {"title":"Return of the Jedi", "episode_id":6,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    },
+    {"title":"The Phantom Menace", "episode_id":1,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    },
+    {"title":"Attack of the Clones", "episode_id":2,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    },
+    {"title":"Revenge of the Sith", "episode_id":3,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    },
+    {"title":"The Force Awakens", "episode_id":7,
+      opening_crawl: "unsure",
+      director: "unsure",
+      producer: "unsure",
+      release_date: "unsure"
+    }
+];
